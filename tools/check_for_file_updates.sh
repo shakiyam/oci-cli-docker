4dd01d7b6257a5482a320ff610759e11069e6f2e
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2025-09-16 06:34:17 +0000

    Add error highlighting to file update detection script

--- tools/check_for_file_updates.sh
+++ tools/check_for_file_updates.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -Eeu -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly SCRIPT_DIR
+# shellcheck disable=SC1091
+. "$SCRIPT_DIR"/colored_echo.sh
+
 if [[ -n $(git diff "$@") ]]; then
+  echo_error "File updates detected:"
   git --no-pager diff "$@"
   exit 2
 fi
