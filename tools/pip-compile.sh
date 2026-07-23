47007973e4ebc183e0556f1ebc111aac002fb007
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2026-07-23 11:16:29 +0000

    Consolidate container engine handling and harden CI

--- tools/pip-compile.sh
+++ tools/pip-compile.sh
@@ -5,26 +5,23 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 readonly SCRIPT_DIR
 # shellcheck disable=SC1091
 . "$SCRIPT_DIR"/colored_echo.sh
+# shellcheck disable=SC1091
+. "$SCRIPT_DIR"/container_engine.sh
 
-if command -v docker &>/dev/null; then
-  docker container run \
-    --name "pip-compile_$(uuidgen | head -c8)" \
-    --rm \
-    -e HOME=/tmp \
-    -u "$(id -u):$(id -g)" \
-    -v "$PWD":/work \
-    -w /work \
-    ghcr.io/shakiyam/pip-tools pip-compile "$@"
-elif command -v podman &>/dev/null; then
-  podman container run \
-    --name "pip-compile_$(uuidgen | head -c8)" \
-    --rm \
-    --security-opt label=disable \
-    -e HOME=/tmp \
-    -v "$PWD":/work \
-    -w /work \
-    ghcr.io/shakiyam/pip-tools pip-compile "$@"
+CONTAINER_ENGINE=$(detect_container_engine)
+readonly CONTAINER_ENGINE
+if [[ $CONTAINER_ENGINE == docker ]]; then
+  ENGINE_OPTS=(-u "$(id -u):$(id -g)")
 else
-  echo_error 'Neither docker nor podman is installed.'
-  exit 1
+  ENGINE_OPTS=(--security-opt label=disable)
 fi
+readonly ENGINE_OPTS
+
+$CONTAINER_ENGINE container run \
+  --name "pip-compile_$(uuidgen | head -c8)" \
+  --rm \
+  "${ENGINE_OPTS[@]}" \
+  -e HOME=/tmp \
+  -v "$PWD":/work \
+  -w /work \
+  ghcr.io/shakiyam/pip-tools pip-compile "$@"
